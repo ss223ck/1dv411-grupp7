41525142434817602d0b34682046ab1e3b5fd4b5
nj222dt <ni.johannesson@gmail.com>
2016-01-29 18:53:13 +0000

    Prototype för applikationen

---
+++ filehandler.js
@@ -0,0 +1,20 @@
+"use strict";
+const file = {
+    
+    init:function(){
+        
+        file.filecreator();
+    },
+    filecreator:function(){
+        const file = document.getElementById("files");
+        file.onchange = function(event){
+            const input = event.target;
+            const filereader = new FileReader();
+            filereader.onload = function(){
+               names.init(JSON.parse(filereader.result));
+            }
+            filereader.readAsText(file.files[0])
+        }
+    },
+}
+window.onload = file.init();
---
+++ getnames.js
@@ -0,0 +1,38 @@
+"use strict";
+const names = {
+    
+    init:function(object){
+        names.getnames(object); 
+    },
+    getnames:function(object){
+        const array = [];
+        const orgname = object.organization;
+        for(var i = 0; i < object.usernames.length; i++){
+            array.push(object.usernames[i]["username"]);
+        }
+        names.urlnames(array,orgname);
+    },
+    urlnames:function(array,orgname){
+        var url = "users/";
+        for(var i = 0; i < array.length; i++){
+            url += array[i] + "+";
+        }
+        url = url.slice(0,-1);
+        names.getnamesfromurl(url,orgname);
+    },
+    getnamesfromurl:function(url,orgname){
+        //makes the request towards api. in real life
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function(){
+            if(xhr.readyState === 4 && xhr.status === 200){
+                var response = JSON.parse(xhr.responseText);
+                names.passiton(response,orgname);
+            }
+        };
+        xhr.open("GET", "names.json", true);
+        xhr.send(null);
+    },
+    passiton:function(response,orgname){
+        gettemplates.init(response,orgname);
+    }
+};
